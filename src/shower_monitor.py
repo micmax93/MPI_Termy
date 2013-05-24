a750a44dcdaa4004018f874142f6fb9b1ef6fd4c
micmax93 <m.elfartas@gmail.com>
2013-05-24 11:51:09 +0200

    Poprawiono nazewnictwo.

--- src/shower_monitor.py
+++ src/shower_monitor.py
@@ -4,20 +4,20 @@ from globals import *
 class ShowerMonitor:
     def __init__(self):
         self.curr_amount = 0
-        self.curr_sex = SEX_FEMALE
+        self.curr_gender = GENDER_FEMALE
 
-    def shower_in(self, sex):
+    def shower_in(self, gender):
         if self.curr_amount == 0:
-            self.curr_sex = sex
+            self.curr_gender = gender
         self.curr_amount += 1
 
     def shower_out(self, ):
         self.curr_amount -= 1
 
-    def check_enter(self, sex):
+    def check_enter(self, gender):
         if self.curr_amount == 0:
             return True
-        elif self.curr_amount < GLOBAL_LOCKER_CAPACITY and self.curr_sex == sex:
+        elif self.curr_amount < GLOBAL_LOCKER_CAPACITY and self.curr_gender == gender:
             return True
         else:
             return False
