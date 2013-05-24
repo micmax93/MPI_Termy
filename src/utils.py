3c07cfad081251d6f4711e8f705f8e5de64640da
Michał Kowalski <mk@x.yum.pl>
2013-05-24 20:16:32 +0200

    Dodano metodę losującą płeć oraz metodę losujacą opóźnienie z ustawionego zakresu.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 from threading import Thread
 import time
+from globals import *
+from random import randint, random
 
 
 def __func_execution(delay, function, args=()):
@@ -10,3 +12,15 @@ def __func_execution(delay, function, args=()):
 def exec_later(delay, function, args=()):
     thread = Thread(target=__func_execution, args=[delay, function, args])
     thread.start()
+
+
+def get_rand_gender():
+    rd = randint(0, 1)
+    if rd == 0:
+        return GENDER_FEMALE
+    else:
+        return GENDER_MALE
+
+
+def get_rand_locker_timeout():
+    return  LOCKER_TIMEOUT[0] + (random() * LOCKER_TIMEOUT[1])
