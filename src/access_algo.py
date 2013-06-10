ba46f7e2f02ffb78e41d2945ec040aab5f3d12d5
micmax93 <m.elfartas@gmail.com>
2013-06-10 22:02:46 +0200

    Ostatnie poprawki oraz zmiana nazwnictwa.

--- src/access_algo.py
+++ src/access_algo.py
@@ -3,7 +3,7 @@ from utils import *
 
 
 class AccessQueue:  # Kolejka oparta o algorytm Lamporta
-    state = 'idle'
+    my_state = 'idle'
     confirmations_num = 0
     confirmations_tab = [False] * mpi_count()
     waiting_set = []
@@ -15,16 +15,16 @@ class AccessQueue:  # Kolejka oparta o algorytm Lamporta
 
     def mk_msg(self, cmd):  #generowanie treści komunikatu
         rank = mpi_rank()
-        data = {'cmd': cmd, 'rank': rank, 'name': self.name, 'tool': 'queue'}
+        data = {'cmd': cmd, 'rank': rank, 'name': self.name}
         return data
 
     def send_request(self):  #wysłanie żądania dostępu do sekcji krytycznej
         data = self.mk_msg('request')
-        self.state = 'waiting'
+        self.my_state = 'waiting'
         self.confirmations_num = 0
         self.confirmations_tab = [False] * mpi_count()
         mpi_bcast(data)
-        say("Request sent ", data)
+        say("Requesting ", self.name, " critical section")
 
     def send_confirmation(self, target):  #wysłanie zezwolenia na wejście do sekcji krytycznej
         data = self.mk_msg('allowed')
@@ -32,9 +32,9 @@ class AccessQueue:  # Kolejka oparta o algorytm Lamporta
         #say("Confirmation sent to ", target)
 
     def exit_critical(self):  #procedura opuszczenia sekcji krytycznej
-        say(">>Exiting ", self.name, " queue section")
-        self.state = 'idle'
-        say(" Sending confirms to: ", self.waiting_set)
+        say(">>Exiting ", self.name, " critical section")
+        self.my_state = 'idle'
+        #say(" Sending confirms to: ", self.waiting_set)
         for e in self.waiting_set:
             self.send_confirmation(e)
         self.waiting_set.clear()
@@ -42,27 +42,27 @@ class AccessQueue:  # Kolejka oparta o algorytm Lamporta
 
     def critical_section(self):  #wywołanie funcji sekcji krytycznej
         #kod sekcji krytycznej
-        say(">>Entering ", self.name, " queue section")
+        say(">>Entering ", self.name, " critical section")
         if self.get_access_func is not None:
             self.get_access_func()
 
     def on_confirmation(self, sender):  #zdarzenie otrzymania potwierdzenia
-        if self.state == 'waiting':
+        if self.my_state == 'waiting':
             if not self.confirmations_tab[sender]:
                 self.confirmations_num += 1
                 self.confirmations_tab[sender] = True
-            say(self.name, " queue confirmation received from ", sender, " - ",
+            say(self.name, " confirmation received from ", sender, " - ",
                 self.confirmations_num, ' out of ', mpi_count() - 1)
             if self.confirmations_num == mpi_count() - 1:
-                self.state = 'active'
+                self.my_state = 'active'
                 self.critical_section()
 
     def on_request(self, sender, data):  #zdarzenie otrzymania żądania dostępu
         #say("Received request from ", sender)
-        if self.state == 'idle':
+        if self.my_state == 'idle':
             self.send_confirmation(sender)
-        elif self.state == 'waiting':
-            if mpi_rank() > sender and not self.confirmations_tab[sender]:
+        elif self.my_state == 'waiting':
+            if mpi_rank() > sender:
                 self.send_confirmation(sender)
             else:
                 self.waiting_set.append(sender)
